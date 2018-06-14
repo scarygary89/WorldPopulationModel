0aad18f8f34cab09b85c129eeb40484564b3c6e6
scarygary89 <glin916@gmail.com>
2018-06-13 22:33:11 -0400

    Update Data

--- ModelCalibration.R
+++ ModelCalibration.R
@@ -5,7 +5,7 @@ library(FME)
 # IMPORT DATA
 
 orgdir = getwd()
-setwd('~/../Dropbox/HumanPopDynModel/Model/R/DataInput')
+setwd('./DataInput')
 
 CalibData_Low = read.csv(  file = 'LowCountries.csv',sep =',',
 						header = T, fileEncoding="UTF-8-BOM")
@@ -34,15 +34,33 @@ RPRatio = c(
 	PF3 = .9,
 	PF4 = .9)
 
-PopData = CalibData_Low[c('Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
+PopDataLow = CalibData_Low[c('Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
 					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female',	
 					 'Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
 					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female')]
 
-PopData = t(RPRatio * t(PopData))
-PopData = cbind(CalibData_Low$Time,PopData)
-colnames(PopData) = c('time','RM1','RM2','RM3','RM4','RF1','RF2','RF3','RF4',
-					'PM1','PM2','PM3','PM4','PF1','PF2','PF3','PF4')
+PopDataMid = CalibData_Mid[c('Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
+					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female',	
+					 'Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
+					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female')]
+
+
+PopDataHigh = CalibData_High[c('Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
+					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female',	
+					 'Pop.1.Male','Pop.2.Male','Pop.3.Male','Pop.4.Male',
+					 'Pop.1.Female','Pop.2.Female','Pop.3.Female','Pop.4.Female')]
+
+PopDataLow = t(RPRatio * t(PopDataLow))
+PopDataMid = t(RPRatio * t(PopDataMid))
+PopDataHigh = t(RPRatio * t(PopDataHigh))
+
+PopData = cbind(CalibData_Low$Time,PopDataLow,PopDataMid,PopDataHigh)
+colnames(PopData) = c('time','Low_RM1','Low_RM2','Low_RM3','Low_RM4','Low_RF1','Low_RF2','Low_RF3','Low_RF4',
+					'Low_PM1','Low_PM2','Low_PM3','Low_PM4','Low_PF1','Low_PF2','Low_PF3','Low_PF4',
+					'Mid_RM1','Mid_RM2','Mid_RM3','Mid_RM4','Mid_RF1','Mid_RF2','Mid_RF3','Mid_RF4',
+					'Mid_PM1','Mid_PM2','Mid_PM3','Mid_PM4','Mid_PF1','Mid_PF2','Mid_PF3','Mid_PF4',
+					'High_RM1','High_RM2','High_RM3','High_RM4','High_RF1','High_RF2','High_RF3','High_RF4',
+					'High_PM1','High_PM2','High_PM3','High_PM4','High_PF1','High_PF2','High_PF3','High_PF4')
 
 
 
@@ -53,11 +71,10 @@ colnames(PopData) = c('time','RM1','RM2','RM3','RM4','RF1','RF2','RF3','RF4',
 # 	obj
 # }
 
-parRanges = read.csv(  file = 'ParameterInput.csv',sep ='|',
-						header = T, fileEncoding="UTF-8-BOM")
-
-# obstime = PopData[,'time']
-obstime = times(WorldMod)
+parRanges = ParameterData[,c('min','max')]
+yobs = na.omit(PopData)
+obstime = yobs[,'time']
+# obstime = times(WorldMod)
 
 ObjCost = function(p, simObj, obstime, yobs){
 	whichpar = names(p)
@@ -67,7 +84,6 @@ ObjCost = function(p, simObj, obstime, yobs){
 	modCost(ysim,yobs)
 }
 
-yobs = PopData
 
 Fit = modFit(p = c(OmegaF_PF1 = 10,OmegaF_PF2 = 10,OmegaF_PF3 = 10), 
 		f = ObjCost, simObj=WorldMod,
